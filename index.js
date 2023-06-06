b115018f8069bf3c48205a44536e9bc7a95fbc73
Delnegend <kiennguyen19323@gmail.com>
2023-06-06 17:57:27 +0700

    refactor(index.js)!: add support for audio, base64
    - the commit summary

--- index.js
+++ index.js
@@ -11,24 +11,39 @@ document.addEventListener('DOMContentLoaded', function () {
     return;
   }
 
-  if (urlValue.startsWith('yt-') || urlValue.startsWith('vimeo-')) {
-    let youtubeVideoId = urlValue.replace('yt-', '');
+  if (urlValue.startsWith('b64-')) {
+    urlValue = atob(urlValue.replace('b64-', ''));
+  }
+
+  if (urlValue.startsWith('yt-') || urlValue.startsWith('vm-')) {
+    let videoId = urlValue.replace('yt-', '') || urlValue.replace('vm-', '');
     let divElement = document.createElement('div');
     divElement.className = 'plyr__video-embed';
     divElement.id = 'player';
 
     let iframeElement = document.createElement('iframe');
     if (urlValue.startsWith('yt-')) {
-      iframeElement.src = `https://www.youtube.com/embed/${youtubeVideoId}?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`;
+      iframeElement.src = `https://www.youtube.com/embed/${videoId}?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`;
     } else if (urlValue.startsWith('vimeo-')) {
       // https://player.vimeo.com/video/76979871?loop=false&amp;byline=false&amp;portrait=false&amp;title=false&amp;speed=true&amp;transparent=0&amp;gesture=media
-      iframeElement.src = `https://player.vimeo.com/video/${youtubeVideoId}?loop=false&amp;byline=false&amp;portrait=false&amp;title=false&amp;speed=true&amp;transparent=0&amp;gesture=media`;
+      iframeElement.src = `https://player.vimeo.com/video/${videoId}?loop=false&amp;byline=false&amp;portrait=false&amp;title=false&amp;speed=true&amp;transparent=0&amp;gesture=media`;
     }
     iframeElement.allowfullscreen = true;
     iframeElement.allowtransparency = true;
     iframeElement.allow = 'autoplay';
     divElement.appendChild(iframeElement);
     document.body.appendChild(divElement);
+  } else if (urlValue.startsWith('audio-')) {
+    let audioElement = document.createElement('audio');
+    audioElement.id = 'player';
+    audioElement.crossOrigin = true;
+    audioElement.playsinline = true;
+
+    let sourceElement = document.createElement('source');
+    sourceElement.src = urlValue.replace('audio-', '');
+
+    audioElement.appendChild(sourceElement);
+    document.body.appendChild(audioElement);
   } else {
     let videoElement = document.createElement('video');
     videoElement.id = 'player';
